2260ee5073937d86d62fd9b87c2362d0c0d08623
lephuc0167283 <68628569+lephuc0167283@users.noreply.github.com>
2024-05-23 00:41:28 +0700

    2
    2

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { AppService } from './app.service';
 // import { AuthMiddleware } from './user/middlewares/auth.middleware'; // Corrected import path
 // import { ProductModule } from './product/product.module';
 // import { ConfigModule } from '@nestjs/config';
-//import { MongooseModule } from '@nestjs/mongoose';
+import { MongooseModule } from '@nestjs/mongoose';
 // import { UserModule } from './user/user.module';
 //import { CartModule } from './cart/cart.module';
 // import { AuthModule } from './auth/auth.module';
@@ -14,7 +14,7 @@ import { AppService } from './app.service';
 
 @Module({
   imports: [
-   // MongooseModule.forRoot('mongodb+srv://blackshop:blackshopmongodb@blackshop.5c90je5.mongodb.net/?retryWrites=true&w=majority&appName=blackshop'),
+    MongooseModule.forRoot('mongodb+srv://blackshop:blackshopmongodb@blackshop.5c90je5.mongodb.net/?retryWrites=true&w=majority&appName=blackshop'),
     //ProductModule,
    // ProductTypeModule,
     //UserModule,
